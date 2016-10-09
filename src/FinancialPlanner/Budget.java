b3235a9d5046b14158eb20046f408f0b49f08950
asl5257 <ardjenlane@hotmail.com>
2016-10-09 18:17:41 -0400

    Added public interface to Budget Created BudgetHandler

--- src/FinancialPlanner/Budget.java
+++ src/FinancialPlanner/Budget.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FinancialPlanner;
 
-/**
- *
- * @author bradcaviston
- */
-public class Budget {
+import java.util.ArrayList;
+
+public class Budget
+{
+    private String name;
+    private String startTime;
+    private String endTime;
+    private double amount;
+    private ArrayList<Transaction> transactions;
     
-    public Budget() {
-        
+    public Budget()
+    {
+        transactions = new ArrayList<Transaction>();
     }
     
+    public void setName(String name){this.name = name;}
+    public void setStartTime(String startTime){this.startTime = startTime;}
+    public void setEndTime(String endTime){this.endTime = endTime;}
+    public void setAmount(double amount){this.amount = amount;}
+    public void setTransactions(ArrayList<Transaction> transactions){this.transactions = transactions;}
     
-    
-    
+    public String getName(){return name;}
+    public String getStartTime(){return startTime;}
+    public String getEndTime(){return endTime;}
+    public double getAmount(){return amount;}
+    public ArrayList<Transaction> getTransactions(){return transactions;}
 }
