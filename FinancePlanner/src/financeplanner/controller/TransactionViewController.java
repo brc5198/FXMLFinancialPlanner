31c5ad028fdf1a7f5b94e7affaeb123f273f7ef0
bradcaviston <bradcaviston@client-104-39-66-226.mobility.psu.edu>
2016-10-30 13:15:58 -0400

    Added the History View and Controller

--- FinancePlanner/src/financeplanner/controller/TransactionViewController.java
+++ FinancePlanner/src/financeplanner/controller/TransactionViewController.java
@@ -51,6 +51,7 @@ public class TransactionViewController
     private void handleSaveButton()
     {
         System.out.println("Save called");
+        requestNewTransaction();
         app.removeWindows();
     }
     
