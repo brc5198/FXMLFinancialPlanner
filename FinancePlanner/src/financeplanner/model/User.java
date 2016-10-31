509a1b0e06a371891a66742dfca66c9ed3717d96
asl5257 <asl5257@110-WL20.up.ist.local>
2016-10-31 14:37:47 -0400

    Added user class

---
+++ FinancePlanner/src/financeplanner/model/User.java
@@ -0,0 +1,24 @@
+package financeplanner.model;
+
+/**
+ *
+ * @author Ardjen
+ */
+public class User 
+{
+    private String firstName;
+    private String lastName;
+    
+    public User()
+    {
+        
+    }
+    
+    //Setters
+    public void setFirstName(String firstName){this.firstName = firstName;}
+    public void setLastName(String lastName){this.lastName = lastName;}
+    
+    //Getters
+    public String getFirstName(){return firstName;}
+    public String getLastName(){return lastName;}
+}
